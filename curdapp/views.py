c3a3e907166a14a61192d646232acd3211c4897b
ganeshchandra111 <ganeshchandracodes@gmail.com>
2024-06-30 15:00:15 +0530

    changed views so that i can accept post

--- curdapp/views.py
+++ curdapp/views.py
@@ -1,60 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse,Http404
-
-from django.http.response import JsonResponse
-from .models import Student
-from .serializers import StudentSerializers
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from .models import Student
+from .serializers import StudentSerializers
 
 class StudentView(APIView):
-    def post(self,request):
-        data = request.data
-        serializer = StudentSerializers(data=data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse('Student saved successfully ',safe=False)
-        else :
-            return JsonResponse('Student saving FALIED',safe=False)
-        
-    def get_student(self,pk):
+    def get_student(self, pk):
         try:
-            student_details = Student.objects.get(studentId=pk)
-            return student_details
+            return Student.objects.get(studentId=pk)
         except Student.DoesNotExist:
             raise Http404
-    
-    def get(self,request,pk=None):
+
+    def get(self, request, pk=None):
         if pk:
             student = self.get_student(pk)
             serializer = StudentSerializers(student)
         else:
             students = Student.objects.all()
-            serializer = StudentSerializers(students,many=True)
-        
+            serializer = StudentSerializers(students, many=True)
         return Response(serializer.data)
 
-
-    def put(self, request, pk=None):
-        student_to_update = Student.objects.get(studentId=pk)
-        serializer = StudentSerializers(instance=student_to_update, data=request.data, partial=True)
-
+    def post(self, request):
+        serializer = StudentSerializers(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            return Response('Student saved successfully', status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-            return JsonResponse("YESSSSSSSSSSSS",safe=False)
-        else:
-            return JsonResponse("NOOOOOOOOOOOOOOOOOO",safe=False)
+    def put(self, request, pk):
+        student = self.get_student(pk)
+        serializer = StudentSerializers(student, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response('Student updated successfully', status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self,request,pk):
-        student_to_delete = Student.objects.get(studentId=pk)
-        student_to_delete.delete()
-
-        return JsonResponse(f"{pk} this guy is deleted",safe=False)
-    
-def home(requests):
-    return HttpResponse('HELOOOOOOOOOOOOOO')
-    #this should be seen on the website
-
+    def delete(self, request, pk):
+        student = self.get_student(pk)
+        student.delete()
+        return Response(f'Student {pk} deleted successfully', status=status.HTTP_204_NO_CONTENT)
