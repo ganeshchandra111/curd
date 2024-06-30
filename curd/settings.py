9afcfdc7c7ed070d34e2ca5aeefd271c79f165ae
ganeshchandra111 <ganeshchandracodes@gmail.com>
2024-06-30 13:16:10 +0530

    added static urls in settings

--- curd/settings.py
+++ curd/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-92)k%j1*%j4esg5=%+a2-k5&h$b2&^5=*i^0-2gxb=w@eyzruh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Application definition
