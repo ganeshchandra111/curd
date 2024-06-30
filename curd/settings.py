23110a23b4514519b6d0c2ddafdb152b23d0d615
ganeshchandra111 <ganeshchandracodes@gmail.com>
2024-06-30 15:26:25 +0530

    fixing post bug4

--- curd/settings.py
+++ curd/settings.py
@@ -124,6 +124,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'ganeshchandra',
+        'PASSWORD': 'idontknow111',
+        'HOST': 'localhost',
     }
 }
 
